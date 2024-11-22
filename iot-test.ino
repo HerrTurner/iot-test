9b14a76d0cec79dfc73105ee5f82040e883dcabd
herrturner <georgeeturnere@gmail.com>
2024-11-21 23:04:21 -0600

    first commit

---
+++ iot-test.ino
@@ -0,0 +1,46 @@
+
+#include "DHT.h"
+#define DHT11_PIN 2
+#define TEMP A0
+
+DHT dht11(DHT11_PIN, DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  dht11.begin(); // initialize the sensor
+}
+
+void loop() {
+  // wait a few seconds between measurements.
+  delay(2000);
+
+  // read humidity
+  float humi  = dht11.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht11.readTemperature();
+  // read temperature as Fahrenheit
+  float tempF = dht11.readTemperature(true);
+
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT11 sensor!");
+  } else {
+    Serial.print("DHT11# Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  "); 
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C ~ ");
+    Serial.print(tempF);
+    Serial.println("°F");
+
+    int val = analogRead(A0);  
+    Serial.println(val);
+    val = analogRead(TEMP)/4095.0*3.3;  
+    Serial.println(val);
+    delay(1000);
+  }
+}
