6ccd1568be846f8a886a9627dd858f08ed0a165a
Jorge E. Turner Escalante <51143651+HerrTurner@users.noreply.github.com>
2024-11-22 02:42:18 -0600

    Update comments

--- test-api/test-api.js
+++ test-api/test-api.js
@@ -12,28 +12,30 @@ function getIPAddress() {
     const interfaces = os.networkInterfaces();
     for (const interfaceName in interfaces) {
         for (const iface of interfaces[interfaceName]) {
-        if (iface.family === "IPv4" && !iface.internal) {
-            return iface.address;
-        }
+            if (iface.family === "IPv4" && !iface.internal) {
+                return iface.address;
+            }
         }
     }
     return "127.0.0.1"; // Fallback address
-    }
+}
 
 
-
+// GET root
 app.get("/", (req, res) => {
     res.send("<h1>IoT ESP32 Test</h1><p>Usa el siguiente endpoint para mandar información:</p><ul><li><b>POST:</b> /api/sensors</li></ul>");
   });
 
-  app.get("/api/sensores", (req, res) => {
+//GET sensores
+app.get("/api/sensores", (req, res) => {
     res.send("<h1>IoT ESP32 Test</h1><p>Revisa la consola</p>");
-  });
-// POST: Add new sensor data
+});
+
+// POST sensores
 app.post("/api/sensores", (req, res) => {
   const  values  = req.body;
   console.log(values);
-  // Validate input
+  // Validar input
   if (values === undefined) {
     return res.status(400).json({
       success: false,
